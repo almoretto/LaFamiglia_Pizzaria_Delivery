66fbe456dee5792f65f3f9f0ce424337600c5d06
amoretto <amoretto@outlook.com.br>
2020-12-12 15:55:52 -0300

    Starting all layers projects, Business, Database, Entities. Created the database conection with MySql.Data

---
+++ LaFamigliaPizzeriaDelivery/DataContext/DbContext.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContext
+{
+    public class DbContext
+    {
+        //Method to create an instance of DBContext Class Localy. 
+        private static readonly DbContext LaFamigliaDbContext = new DbContext();
+
+        //Method to return the instance created above
+        public static DbContext GetInstance()
+        {
+            return LaFamigliaDbContext;
+        }
+
+        //Method to create the DB Connection
+        public MySqlConnection GetConnection()
+        {
+            //This line gets the ConnectionString from app.config file.
+            string dbConnection = ConfigurationManager.ConnectionStrings["DataContextConection"].ToString();
+            return new MySqlConnection(dbConnection);
+        }
+    }
+}
