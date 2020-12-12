5bf6ef29ad4b15a128a1177378fe4010a6880823
amoretto <amoretto@outlook.com.br>
2020-12-12 18:32:33 -0300

    Added Resources  to project, created FormLogin layout Configured MDIForm, and initiate design

---
+++ LaFamigliaPizzeriaDelivery/UserInterface/Login/FrmLogin.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace UserInterface.Login
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.pnlShadow = new System.Windows.Forms.Panel();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.cmbUsers = new System.Windows.Forms.ComboBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Title = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CopyRight = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pctLogo
+            // 
+            this.pctLogo.BackgroundImage = global::UserInterface.Properties.Resources.Logo;
+            this.pctLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctLogo.Location = new System.Drawing.Point(744, 26);
+            this.pctLogo.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.Size = new System.Drawing.Size(225, 300);
+            this.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctLogo.TabIndex = 0;
+            this.pctLogo.TabStop = false;
+            // 
+            // pnlShadow
+            // 
+            this.pnlShadow.AllowDrop = true;
+            this.pnlShadow.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pnlShadow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlShadow.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pnlShadow.Location = new System.Drawing.Point(754, 36);
+            this.pnlShadow.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.pnlShadow.Name = "pnlShadow";
+            this.pnlShadow.Size = new System.Drawing.Size(225, 300);
+            this.pnlShadow.TabIndex = 1;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.BackColor = System.Drawing.SystemColors.Window;
+            this.lblUser.Location = new System.Drawing.Point(612, 363);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(62, 22);
+            this.lblUser.TabIndex = 2;
+            this.lblUser.Text = "Login";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.lblPassword.Location = new System.Drawing.Point(612, 395);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(68, 22);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Senha";
+            // 
+            // cmbUsers
+            // 
+            this.cmbUsers.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cmbUsers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUsers.FormattingEnabled = true;
+            this.cmbUsers.Location = new System.Drawing.Point(710, 361);
+            this.cmbUsers.Name = "cmbUsers";
+            this.cmbUsers.Size = new System.Drawing.Size(336, 29);
+            this.cmbUsers.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(710, 396);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(336, 29);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(710, 441);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(155, 40);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightCoral;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(891, 441);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(155, 40);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Sair";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Moccasin;
+            this.panel1.Controls.Add(this.CopyRight);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Location = new System.Drawing.Point(616, 504);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 143);
+            this.panel1.TabIndex = 7;
+            // 
+            // Title
+            // 
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Title.Font = new System.Drawing.Font("Futura Bk BT", 14.26415F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(6, 9);
+            this.Title.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(418, 38);
+            this.Title.TabIndex = 3;
+            this.Title.Text = "La Famiglia Pizzeria Delivery System";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Wheat;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Futura Bk BT", 10.18868F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(288, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Version: 1.0 - Beta";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CopyRight
+            // 
+            this.CopyRight.Font = new System.Drawing.Font("Futura Bk BT", 8.830189F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopyRight.Location = new System.Drawing.Point(8, 119);
+            this.CopyRight.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.CopyRight.Name = "CopyRight";
+            this.CopyRight.Size = new System.Drawing.Size(416, 17);
+            this.CopyRight.TabIndex = 5;
+            this.CopyRight.Text = "₢ Moretto Code Studio - 2020";
+            this.CopyRight.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UserInterface.Properties.Resources.fundo_login2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1063, 659);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.cmbUsers);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.pctLogo);
+            this.Controls.Add(this.pnlShadow);
+            this.Font = new System.Drawing.Font("Futura Bk BT", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "FrmLogin";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Panel pnlShadow;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.ComboBox cmbUsers;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label CopyRight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Title;
+    }
+}
---
+++ LaFamigliaPizzeriaDelivery/UserInterface/MDIForm.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace UserInterface
+{
+    partial class MDIForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MDIForm));
+            this.SuspendLayout();
+            // 
+            // MDIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1602, 1039);
+            this.Font = new System.Drawing.Font("Futura Md BT", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "MDIForm";
+            this.Text = "La Famiglia Pizzeria Delivery";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MDIForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
