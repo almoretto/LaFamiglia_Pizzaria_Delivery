6116f2d03cda8a78a0b6b772680d1f1e3ea829cd
amoretto <amoretto@outlook.com.br>
2020-12-20 09:03:56 -0300

    Created Control Design to Status

--- LaFamigliaPizzeriaDelivery/UserInterface/Controls/UserSituacaoCTRL.Designer.cs
+++ LaFamigliaPizzeriaDelivery/UserInterface/Controls/UserSituacaoCTRL.Designer.cs
@@ -28,10 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.grpStatus = new System.Windows.Forms.GroupBox();
+            this.rbtActive = new System.Windows.Forms.RadioButton();
+            this.rbtInactive = new System.Windows.Forms.RadioButton();
+            this.grpStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpStatus
+            // 
+            this.grpStatus.Controls.Add(this.rbtInactive);
+            this.grpStatus.Controls.Add(this.rbtActive);
+            this.grpStatus.Location = new System.Drawing.Point(20, 14);
+            this.grpStatus.Name = "grpStatus";
+            this.grpStatus.Size = new System.Drawing.Size(162, 47);
+            this.grpStatus.TabIndex = 0;
+            this.grpStatus.TabStop = false;
+            this.grpStatus.Text = "Status";
+            // 
+            // rbtActive
+            // 
+            this.rbtActive.AutoSize = true;
+            this.rbtActive.Location = new System.Drawing.Point(15, 19);
+            this.rbtActive.Name = "rbtActive";
+            this.rbtActive.Size = new System.Drawing.Size(50, 19);
+            this.rbtActive.TabIndex = 0;
+            this.rbtActive.TabStop = true;
+            this.rbtActive.Text = "Ativo";
+            this.rbtActive.UseVisualStyleBackColor = true;
+            this.rbtActive.CheckedChanged += new System.EventHandler(this.rbtActive_CheckedChanged);
+            // 
+            // rbtInactive
+            // 
+            this.rbtInactive.AutoSize = true;
+            this.rbtInactive.Location = new System.Drawing.Point(82, 19);
+            this.rbtInactive.Name = "rbtInactive";
+            this.rbtInactive.Size = new System.Drawing.Size(60, 19);
+            this.rbtInactive.TabIndex = 1;
+            this.rbtInactive.TabStop = true;
+            this.rbtInactive.Text = "Inativo";
+            this.rbtInactive.UseVisualStyleBackColor = true;
+            this.rbtInactive.CheckedChanged += new System.EventHandler(this.rbtInactive_CheckedChanged);
+            // 
+            // UserSituacaoCTRL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grpStatus);
+            this.Name = "UserSituacaoCTRL";
+            this.Size = new System.Drawing.Size(199, 69);
+            this.Load += new System.EventHandler(this.UserSituacaoCTRL_Load);
+            this.grpStatus.ResumeLayout(false);
+            this.grpStatus.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpStatus;
+        private System.Windows.Forms.RadioButton rbtInactive;
+        private System.Windows.Forms.RadioButton rbtActive;
     }
 }
--- LaFamigliaPizzeriaDelivery/UserInterface/Controls/UserSituacaoCTRL.cs
+++ LaFamigliaPizzeriaDelivery/UserInterface/Controls/UserSituacaoCTRL.cs
@@ -1,20 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entities.Enums;
 
 namespace UserInterface.Controls
 {
     public partial class UserSituacaoCTRL : UserControl
     {
+        public Status CurrentStatus;
         public UserSituacaoCTRL()
         {
             InitializeComponent();
+        }
+        private void UserSituacaoCTRL_Load(object sender, EventArgs e)
+        {
+            if (DesignMode) { return; }
+            StartStatus(Status.Ativo);
+        }
+        private void StartStatus(Status status)
+        {
+            if (DesignMode) { return; }
+            CurrentStatus = status;
+            if (CurrentStatus == Status.Ativo)
+            {
+                rbtActive.Checked = true;
+            }
+            else
+            {
+                rbtInactive.Checked = true;
+            }
+        }
+        private void rbtActive_CheckedChanged(object sender, EventArgs e)
+        {
+            if (DesignMode) { return; }
+            if (rbtInactive.Checked)
+            {
+                CurrentStatus = Status.Ativo;
+            }
+        }
+        private void rbtInactive_CheckedChanged(object sender, EventArgs e)
+        {
+            if (DesignMode) { return; }
+            if (rbtInactive.Checked)
+            {
+                CurrentStatus = Status.Inativo;
+            }
         }
     }
 }
