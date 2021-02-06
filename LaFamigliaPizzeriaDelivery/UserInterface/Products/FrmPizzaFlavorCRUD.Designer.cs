862c399e68b4e9b55c0dcb0488dbc4ebb955fe1e
amoretto <amoretto@outlook.com.br>
2021-02-06 11:13:45 -0300

    Pizza Flavor Crud Start

---
+++ LaFamigliaPizzeriaDelivery/UserInterface/Products/FrmPizzaFlavorCRUD.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace UserInterface.Products
+{
+    partial class FrmPizzaFlavorCRUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPizzaFlavorCRUD));
+            this.SuspendLayout();
+            // 
+            // FrmPizzaFlavorCRUD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPizzaFlavorCRUD";
+            this.Text = "FrmPizzaFlavor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
